21ccef57cda0b1d1ce42409bef5616208bd9578e
Alexperancho <109291631+Alexperancho@users.noreply.github.com>
2025-08-21 17:15:05 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js — Pega aquí la URL pública de tu servidor Socket.IO
+// Ejemplos:
+//   Railway: "https://tu-proyecto.up.railway.app"
+//   Render:  "https://tuapp.onrender.com"
+window.SG_SERVER = "https://survival-game-multiplayer-production.up.railway.app/";
